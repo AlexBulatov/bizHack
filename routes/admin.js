4023e4ec40384b97ac3ba74575d4d4c516b7ac88
Danila Kryukov <danila.kryukov.dev@yandex.ru>
2019-05-14 19:08:30 +0700

    minor fixes and sintax

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,15 @@ const {client} = require('../db');
 
 
 router.get('/', async (req, res) => { 
+    
+    const limit = typeof(req.body.limit) == 'undefined' ? req.body.limit : 20;
+    const offset = typeof(req.body.offset) == 'undefined' ? req.body.offset : 0;
+    
     client.connect();
-    let result = await client.query(`SELECT pilot, departure, arrival, flight FROM flies;`);
+    let result = await client.query(`SELECT pilot, departure, arrival, flight FROM flies LIMIT  ${limit} OFFSET ${offset};`);
     let result2 = await client.query(`SELECT from_date, to_date, id, flight FROM flyscale;`);
     client.end();
+
     res.send(result.rows,result2.rows);
     
 });
@@ -26,6 +31,24 @@ router.put('/flyscale', async (req,res )=>{
 
     client.connect();
     await client.query(`UPDATE flyscale SET from_date = ${from_date.toPostgers()} , to_date = ${to_date.toPostgers()}, days = ${days} WHERE id = ${id};`);
+    client.end();
+
+    res.send('SUCCESS');
+});
+
+router.put('/flies', async (req,res )=>{
+    
+    const id = req.body.id;
+    const departure = req.body.departure;
+    const arrival =req.body.arrival;
+    const flight =req.body.flight;
+
+    if(typeof(departure) != 'string' || typeof(arrival) != 'string' || typeof(flight) != 'number')
+        return res.status(401).send("No Passwd or id");
+
+
+    client.connect();
+    await client.query(`UPDATE flies SET departure = ${departure} , arrival = ${arrival}, flight = ${flight} WHERE id = ${id};`);
     client.end();
 
     res.send('SUCCESS');
