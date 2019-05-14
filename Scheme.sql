dec36e3fc78dd720c60ee21f069bb15c3779b974
AlexBulatov <al.ross@yandex.ru>
2019-05-14 13:05:33 +0700

    Initial commit

---
+++ Scheme.sql
@@ -0,0 +1,34 @@
+CREATE TABLE users( 
+    id serial primary key, 
+    fio varchar, 
+    password varchar, 
+    role int
+);
+
+CREATE TABLE flyscale (
+    id serial primary key, 
+    from_date interval, 
+    to_date interval, 
+    days int
+);
+
+CREATE TABLE flies (
+    id serial primary key,
+    pilot int REFERENCES users(id),
+    departure timestamp,
+    arrival timestamp,
+    flight varchar
+);
+
+CREATE TABLE vacation (
+    id serial primary key,
+    pilot int REFERENCES users(id),
+    from_date date,
+    to_date date
+);
+
+SELECT fio, f.flyhours/EXTRACT(DOY FROM current_date) as 'hours'
+    FROM users JOIN
+    (SELECT id, SUM(arrival - departure) as flyhours 
+    FROM flies
+    GROUP BY id) f.id=users.id; 
